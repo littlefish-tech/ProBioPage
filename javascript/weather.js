86d8cac7cbbb91855948251fc46d4257336aea73
Yimeng Yu <yimeng@Yimengs-MBP.lan>
2019-11-21 20:51:02 -0800

    update html format

---
+++ javascript/weather.js
@@ -0,0 +1,20 @@
+//query url for pollen
+
+function callpollen() {
+       
+    var queryURL = "https://api.waqi.info/feed/here/?token=12f820d56fa3fd40bd4af15eae5097c9875e7bc5";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response){
+        console.log(response)
+        var weatherTemp = response.data.iaqi.t.v;
+        console.log(weatherTemp);
+        //Different ranges for AQI (air quality index)
+       
+        $(".weather").append(JSON.stringify(response.data.iaqi.t.v));
+        //$("#pollen").append(notes);
+        
+    })
+}
+callpollen()
